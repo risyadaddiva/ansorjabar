1dddac70b4c766f0732eebbf2822bb1bde3a5f6b
risyadaddiva <risyadaddiva@gmail.com>
2026-07-31 23:47:45 +0700

    fix: tambahkan konfigurasi turbopack.root di next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
   images: {
     remotePatterns: [
       {
